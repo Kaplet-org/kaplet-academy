b12f6cd96a1411d3f82b54e879a9e8649e7df0c3
Kaplet-org <admin@kaplet.it>
2026-05-04 16:01:15 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,6 +6,7 @@ const KAPLET_CONFIG = {
   supabase: {
     url: 'https://mcgerrvorboagkukzuzc.supabase.co',
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1jZ2VycnZvcmJvYWdrdWt6dXpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc4NjEyNzIsImV4cCI6MjA5MzQzNzI3Mn0.eDHZ-QUQLM2tFU4KPDEiIqXZsdfk_Cxnz7Ywxt6T46o'
+  },
   email: {
     mittente: 'admin@kaplet.it',
     destinatario: 'admin@kaplet.it',
